607755e34afdcfa3d62d7c41d3f61e8a31891c93
Jakub Jóźwiak <jakub.lazy@gmail.com>
2022-01-05 11:43:42 +0100

    useReducer and add own hook

--- src/views/AddUser.js
+++ src/views/AddUser.js
@@ -1,60 +1,38 @@
-import React, { useContext, useReducer } from 'react';
+import React, { useContext } from 'react';
 import FormField from 'components/molecules/FormField/FormField';
 import { Button } from 'components/atoms/Button/Button';
 import { Title } from 'components/atoms/Title/Title';
 import { ViewWrapper } from 'components/molecules/ViewWrapper/ViewWrapper.styles';
 import { UsersContext } from 'providers/UsersProvider';
+import { useForm } from 'hooks/useForm';
 
 //Started object values
 const initialFormState = {
   name: '',
   attendance: '',
   average: '',
-};
-
-//Usereducer similar method like useState but in reducer we can change more information
-const reducer = (state, action) => {
-  //State and action - two attributesm STATE
-  console.log(action);
-
-  switch (action.type) {
-    case 'INPUT CHANGE':
-      //Set values from input
-      return {
-        ...state,
-        [action.field]: action.value,
-      };
-    case 'CLEAR VALUES':
-      return initialFormState;
-    default:
-      return state;
-  }
+  consent: false,
+  error: '',
 };
 
 const AddUser = () => {
-  //Hook
-  const [formValues, dispatch] = useReducer(reducer, initialFormState);
-
   //"Import" function by useContext from another file
   const { handleAddUser } = useContext(UsersContext);
-
-  const handleInputChange = (e) => {
-    //Download new values from input and set them to formValues
-    dispatch({
-      type: 'INPUT CHANGE',
-      field: e.target.name,
-      value: e.target.value,
-    });
-  };
+  const { formValues, handleClearForm, handleToggleConsent, handleThrowError, handleInputChange } = useForm(initialFormState);
 
   const handleSubmitUser = (e) => {
     e.preventDefault();
 
-    //Adding new values to our users list
-    handleAddUser(formValues);
+    if (formValues.consent) {
+      //Adding new values to our users list
+      handleAddUser(formValues);
 
-    //set Initial form values - clear input
-    dispatch({ type: 'CLEAR VALUES' });
+      //set Initial form values - clear input
+      handleClearForm(initialFormState);
+    } else {
+      //Throw error
+      handleThrowError('You need to give consent');
+    }
   };
 
   return (
@@ -63,7 +41,9 @@ const AddUser = () => {
       <FormField label="Name" id="name" name="name" value={formValues.name} onChange={handleInputChange} />
       <FormField label="Attendance" id="attendance" name="attendance" value={formValues.attendance} onChange={handleInputChange} />
       <FormField label="Average" id="average" name="average" value={formValues.average} onChange={handleInputChange} />
+      <FormField label="Consent" id="consent" name="consent" type="checkbox" onChange={handleToggleConsent}></FormField>
       <Button type="submit">Add</Button>
+      {formValues.error ? <p>{formValues.error}</p> : null}
     </ViewWrapper>
   );
 };
